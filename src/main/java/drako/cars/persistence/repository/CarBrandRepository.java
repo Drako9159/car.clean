fd74da760c25ffe286dc23d4971a7a0e4dc7f91d
drako9159 <chochito9159@gmail.com>
2023-10-28 21:23:07 -0600

    complete repository

--- src/main/java/drako/cars/persistence/repository/CarBrandRepository.java
+++ src/main/java/drako/cars/persistence/repository/CarBrandRepository.java
@@ -2,6 +2,7 @@ package drako.cars.persistence.repository;
 
 import drako.cars.domain.pojo.CarBrandPojo;
 import drako.cars.domain.repository.ICarBrandRepository;
+import drako.cars.persistence.entity.CarBrandEntity;
 import drako.cars.persistence.mapper.ICarBrandMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -26,24 +27,46 @@ public class CarBrandRepository implements ICarBrandRepository {
      */
     private final ICarBrandMapper iCarBrandMapper;
 
+    /**
+     * Return all CarBrand
+     *
+     * @return list of CarBrand
+     */
     @Override
     public List<CarBrandPojo> getAll() {
         return iCarBrandMapper.toCarBrandsPojo(iCarBrandCrudRepository.findAll());
     }
 
+    /**
+     * Return a CarBrand by id
+     *
+     * @param id
+     * @return CarBrand
+     */
     @Override
     public Optional<CarBrandPojo> getCarBrand(Integer id) {
         return iCarBrandCrudRepository.findById(id).map(iCarBrandMapper::toCarBrandPojo);
     }
 
+    /**
+     * Save a new CarBrand
+     *
+     * @param newCarBrand CarBrand to save
+     * @return CarBrand created
+     */
     @Override
     public CarBrandPojo save(CarBrandPojo newCarBrand) {
-
-        return null;
+        CarBrandEntity carBrandEntity = iCarBrandMapper.toCarBrandEntity(newCarBrand);
+        return iCarBrandMapper.toCarBrandPojo(iCarBrandCrudRepository.save(carBrandEntity));
     }
 
+    /**
+     * Delete a CarBrand
+     *
+     * @param idCarBrand CarBrand to delete
+     */
     @Override
     public void delete(Integer idCarBrand) {
-
+        iCarBrandCrudRepository.deleteById(idCarBrand);
     }
 }
