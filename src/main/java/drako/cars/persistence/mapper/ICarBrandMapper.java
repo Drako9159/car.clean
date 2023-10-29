a30a5b52212659388a0ad9333022f33a277ed17d
drako9159 <chochito9159@gmail.com>
2023-10-28 20:00:38 -0600

    create compilation mapper

--- src/main/java/drako/cars/persistence/mapper/ICarBrandMapper.java
+++ src/main/java/drako/cars/persistence/mapper/ICarBrandMapper.java
@@ -6,14 +6,40 @@ import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
+/**
+ * Mapper for transform objets CardBrand
+ * Pojo to Entity
+ * Entity to Pojo
+ */
 @Mapper(componentModel = "spring")
 public interface ICarBrandMapper {
 
+    /**
+     * Convert entity to CarBrandPojo
+     *
+     * @param brandEntity entity to convert
+     * @return pojo converted
+     */
     @Mapping(source = "id", target = "id")
     @Mapping(source = "description", target = "description")
     CarBrandPojo toCarBrandPojo(CarBrandEntity brandEntity);
 
+    /**
+     * Convert pojo to CarBrandEntity
+     *
+     * @param brandPojo pojo to convert
+     * @return entity converted
+     */
     @InheritInverseConfiguration
     CarBrandEntity toCarBrandEntity(CarBrandPojo brandPojo);
 
+    /**
+     * Get list of car brand transformed to pojo of list entities
+     *
+     * @param carBrandsEntity Entity for transform
+     * @return list transformed
+     */
+    List<CarBrandPojo> toCarBrandsPojo(List<CarBrandEntity> carBrandsEntity);
 }
