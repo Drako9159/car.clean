1e19e96e3905607d3390c2c6ffa42bdf5600973b
drako9159 <chochito9159@gmail.com>
2023-10-28 21:54:47 -0600

    preparing controller for car-brands path

---
+++ src/main/java/drako/cars/controller/CarBrandController.java
@@ -0,0 +1,12 @@
+package drako.cars.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/car-brands")
+public class CarBrandController {
+
+    
+
+}
